00282bfa74526581b325cb3c8497b2d18b6b6189
Chinmay Mhatre <chinmaymhatre111@gmail.com>
2024-04-11 20:46:07 +0530

    add trash button

--- app/components/Submitbuttons.tsx
+++ app/components/Submitbuttons.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
+import { Loader2, Trash } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
 export function SubmitButton() {
@@ -57,5 +57,23 @@ export function StripePortal() {
             </Button>
         ) }
       </>
+  )
+}
+
+export function TrashDelete() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      { pending ? (
+          <Button variant='destructive' size='icon' disabled>
+            <Loader2 className="h-4 w-4 animate-spin" />
+          </Button>
+      ) : (
+        <Button variant='destructive' size='icon' type="submit">
+          <Trash className="h-4 w-4" />
+        </Button>
+      ) }
+    </>
   )
 }
