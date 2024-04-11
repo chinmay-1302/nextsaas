c1a87346f820b00a5e35ad50a27e14fcf891fb34
Chinmay Mhatre <chinmaymhatre111@gmail.com>
2024-04-11 21:52:19 +0530

    update billing paths

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -61,7 +61,9 @@ export default async function BillingPage() {
 
     const subscriptionUrl = await getStripeSession({
       customerId: dbUser.stripeCustomerId,
-      domainUrl: 'http://localhost:3000',
+      domainUrl: process.env.NODE_ENV === 'production'
+        ? (process.env.PRODUCTION_URL as string)
+        : 'http://localhost:3000',
       priceId: process.env.STRIPE_PRICE_ID as string,
     });
 
@@ -72,7 +74,9 @@ export default async function BillingPage() {
     "use server"
     const session = await stripe.billingPortal.sessions.create({
       customer: data?.user.stripeCustomerId as string,
-      return_url: "http://localhost:3000/dashboard",
+      return_url: process.env.NODE_ENV === 'production'
+        ? (process.env.PRODUCTION_URL as string)
+        : 'http://localhost:3000/dashboard',
     });
 
     return redirect(session.url);
